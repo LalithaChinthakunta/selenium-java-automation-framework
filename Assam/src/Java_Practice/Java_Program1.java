4002f491d34a49462f1cec5672f18d4e249da799
Lalitha Chinthakunta <chinthakuntalalitha244@gmail.com>
2026-04-23 19:32:29 +0530

    Initial Commit

---
+++ Assam/src/Java_Practice/Java_Program1.java
@@ -0,0 +1,9 @@
+package Java_Practice;
+
+public class Java_Program1 {
+
+	public static void main(String[] args) {
+System.out.println("Run");
+	}
+
+}
---
+++ Selenium_Java_GitHub/src/main/java/Java_Practice_GitHub/My_First_Java_Program.java
@@ -0,0 +1,9 @@
+package Java_Practice_GitHub;
+
+public class My_First_Java_Program {
+
+	public static void main(String[] args) {
+		System.out.println("My First Java Program");
+	}
+
+}
